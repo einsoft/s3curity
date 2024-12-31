460b92df43e31322ea56dc333017f70331245692
einstein <einsoft@gmail.com>
2024-12-31 10:07:15 -0300

    feature: adição dos módulos de autenticação e banco de dados com endpoint de login (retornando apenas a string 'login')

--- apps/backend/dist/app.module.js
+++ apps/backend/dist/app.module.js
@@ -9,15 +9,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const auth_module_1 = require("./auth/auth.module");
+const db_module_1 = require("./db/db.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [auth_module_1.AuthModule, db_module_1.DbModule],
         controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
