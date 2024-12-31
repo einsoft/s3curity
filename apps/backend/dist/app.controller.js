7b50bc47c9654100a93b433048ee37f765b8d01c
einstein <einsoft@gmail.com>
2024-12-31 09:48:09 -0300

    feature: disponibilização rota 'ping' no backend

--- apps/backend/dist/app.controller.js
+++ apps/backend/dist/app.controller.js
@@ -11,24 +11,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
-const app_service_1 = require("./app.service");
 let AppController = class AppController {
-    constructor(appService) {
-        this.appService = appService;
-    }
     getHello() {
-        return this.appService.getHello();
+        return 'Ping? Pong!';
     }
 };
 exports.AppController = AppController;
 __decorate([
-    (0, common_1.Get)(),
+    (0, common_1.Get)('ping'),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", String)
 ], AppController.prototype, "getHello", null);
 exports.AppController = AppController = __decorate([
-    (0, common_1.Controller)(),
-    __metadata("design:paramtypes", [app_service_1.AppService])
+    (0, common_1.Controller)()
 ], AppController);
 //# sourceMappingURL=app.controller.js.map
