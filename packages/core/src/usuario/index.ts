a51bddb15b63d80937e210823fb3bc700d14f82e
einstein <einsoft@gmail.com>
2025-01-07 00:18:28 -0300

    refactor: adicione BcryptProvider para criptografia de senha e atualize a lógica de registro do usuário

--- packages/core/src/usuario/index.ts
+++ packages/core/src/usuario/index.ts
@@ -1,6 +1,7 @@
 import Usuario from "./model/Usuario";
 import RepositorioUsuario from "./provider/RepositorioUsuario";
 import RegistrarUsuario from "./service/RegistrarUsuario";
+import ProvedorCriptografia from "./provider/ProvedorCriptogafia";
 
-export type { Usuario, RepositorioUsuario };
+export type { Usuario, RepositorioUsuario, ProvedorCriptografia };
 export { RegistrarUsuario };
