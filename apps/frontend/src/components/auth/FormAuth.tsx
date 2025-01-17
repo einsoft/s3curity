ed4b28372e2fe4dcd123c5d6b83c9d77e7a7ac6b
einstein <einsoft@gmail.com>
2025-01-17 09:56:38 -0300

    feat: base para form, aternando entre cadastro e login

--- apps/frontend/src/components/auth/FormAuth.tsx
+++ apps/frontend/src/components/auth/FormAuth.tsx
@@ -1,11 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import logo from "../../assets/images/logo.svg";
+
 export default function FormAuth() {
+  const [modo, setModo] = useState<"login" | "cadastro">("login");
+
+  function alternarModo() {
+    setModo(modo === "login" ? "cadastro" : "login");
+  }
+
   return (
     <div className="flex justify-center items-center h-screen">
-      Formulário de Autenticação
+      <Image src={logo} width={150} height={150} alt="Logo" />
       <div className="flex flex-col">
+        {modo === "cadastro" && (
+          <>
+            <input type="text" placeholder="Nome Completo" />
+            <input type="text" placeholder="Telefone" />
+          </>
+        )}
         <input type="text" placeholder="E-mail" />
         <input type="password" placeholder="Senha" />
-        <button>Entrar</button>
+        <button>Confirmar</button>
+      </div>
+      <div>
+        <button onClick={alternarModo}>
+          {modo === "login" ? (
+            <span>Aindão não tem conta? Cadastre-se</span>
+          ) : (
+            <span>Já tem uma conta? Faça o Login</span>
+          )}
+        </button>
       </div>
     </div>
   );
