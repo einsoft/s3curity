dd21dd61be3b817e471f3c52ea1d1a51ca78aa07
einstein <einsoft@gmail.com>
2025-01-17 12:28:52 -0300

    feat: login retornando o JWT

--- apps/frontend/src/components/auth/FormAuth.tsx
+++ apps/frontend/src/components/auth/FormAuth.tsx
@@ -3,27 +3,64 @@
 import { useState } from "react";
 import Image from "next/image";
 import logo from "../../assets/images/logo.svg";
+import useAPI from "@/src/data/hooks/useAPI";
 
 export default function FormAuth() {
   const [modo, setModo] = useState<"login" | "cadastro">("login");
+
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const { httpPost } = useAPI();
 
   function alternarModo() {
     setModo(modo === "login" ? "cadastro" : "login");
   }
 
+  async function submeter() {
+    if (modo === "login") {
+      const token = await httpPost("/auth/login", { email, senha });
+      console.log(token);
+    } else {
+      console.log({ email, senha });
+    }
+  }
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <Image src={logo} width={150} height={150} alt="Logo" />
+      <Image src={logo} width={150} height={150} alt="Logo" priority={false} />
       <div className="flex flex-col">
         {modo === "cadastro" && (
           <>
-            <input type="text" placeholder="Nome Completo" />
-            <input type="text" placeholder="Telefone" />
+            <input
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              placeholder="Nome Completo"
+            />
+            <input
+              type="text"
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
+              placeholder="Telefone"
+            />
           </>
         )}
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button>Confirmar</button>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail"
+        />
+        <input
+          type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Senha"
+        />
+        <button onClick={submeter}>Confirmar</button>
       </div>
       <div>
         <button onClick={alternarModo}>
