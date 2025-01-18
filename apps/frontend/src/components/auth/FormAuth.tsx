36e9fcd0d30b2ed5fc9bc86d09aa372fade44ec7
einstein <einsoft@gmail.com>
2025-01-17 21:10:41 -0300

    style: form login frontend

--- apps/frontend/src/components/auth/FormAuth.tsx
+++ apps/frontend/src/components/auth/FormAuth.tsx
@@ -56,35 +56,71 @@ export default function FormAuth() {
         <Logo />
       </div>
       {modo === "cadastro" && (
-        <div className="formulario">
-          <input
-            type="text"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            placeholder="Nome Completo"
-          />
-          <input
-            type="text"
-            value={telefone}
-            onChange={(e) => setTelefone(e.target.value)}
-            placeholder="Telefone"
-          />
+        <div className="espacamento_superior">
+          <div className="formulario">
+            <label className="labelFormLogin" htmlFor="nome">
+              Nome Completo
+            </label>
+            <input
+              id="nome"
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              placeholder="Nome Completo"
+              className="inputFormLogin"
+            />
+          </div>
+          <div className="espacamento_superior">
+            <label className="labelFormLogin" htmlFor="telefone">
+              Telefone
+            </label>
+            <input
+              id="nome"
+              type="text"
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
+              placeholder="Telefone"
+              className="inputFormLogin"
+            />
+          </div>
         </div>
       )}
       <div className="formulario">
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail"
-        />
-        <input
-          type="password"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          placeholder="Senha"
-        />
-        <button onClick={submeter}>Confirmar</button>
+        <div className="espacamento_superior">
+          <label className="labelFormLogin" htmlFor="email">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail"
+            className="inputFormLogin"
+          />
+        </div>
+        <div className="espacamento_superior">
+          <label
+            className="labelFormLogin espacamento_superior"
+            htmlFor="senha"
+          >
+            Senha
+          </label>
+          <input
+            id="senha"
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Senha"
+            className="inputFormLogin"
+          />
+        </div>
+        <button
+          onClick={submeter}
+          className="button_green espacamento_superior"
+        >
+          Confirmar
+        </button>
       </div>
       <div className="espacamento_superior">
         <button onClick={alternarModo}>
