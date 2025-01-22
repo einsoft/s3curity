6c8884b3d278d92ba7f1f25c5f12cb92d1c8b546
Albert E. Souza <einsoft@gmail.com>
2025-01-22 14:50:49 -0300

    style: format code for consistency and readability across multiple files

--- packages/core/src/usuario/provider/RepositorioUsuario.ts
+++ packages/core/src/usuario/provider/RepositorioUsuario.ts
@@ -2,7 +2,7 @@ import Usuario from "../model/Usuario";
 
 export default interface RepositorioUsuario {
   salvar(usuario: Usuario): Promise<void>;
-  buscarPorEmail(email: string): Promise<any>;  
+  buscarPorEmail(email: string): Promise<any>;
   buscarPorId(id: number): Promise<any>;
   atualizarNome(id: number, nomeCompleto: string): Promise<void>;
   atualizarSenha(id: number, novaSenha: string): Promise<void>;
--- packages/core/src/usuario/service/AtualizarSenhaUsuario.ts
+++ packages/core/src/usuario/service/AtualizarSenhaUsuario.ts
@@ -1,4 +1,3 @@
-
 import CasoDeUso from "../../shared/CasoDeUso";
 import Usuario from "../model/Usuario";
 import ProvedorCriptografia from "../provider/ProvedorCriptogafia";
@@ -9,35 +8,36 @@ type Entrada = {
   senhaAtual: string;
   novaSenha: string;
   confirmaNovaSenha: string;
-  usuarioLogado: Usuario
+  usuarioLogado: Usuario;
 };
 
 export default class AtualizarSenhaUsuario implements CasoDeUso<Entrada, void> {
   constructor(
     private readonly repo: RepositorioUsuario,
-    private readonly cripto: ProvedorCriptografia
+    private readonly cripto: ProvedorCriptografia,
   ) {}
 
   async executar(entrada: Entrada): Promise<void> {
-    const { id, senhaAtual, novaSenha, confirmaNovaSenha, usuarioLogado } = entrada;
-   
-    if (novaSenha?.length <= 3){
+    const { id, senhaAtual, novaSenha, confirmaNovaSenha, usuarioLogado } =
+      entrada;
+
+    if (novaSenha?.length <= 3) {
       throw new Error("Senha inválido");
     }
 
-    if(novaSenha !== confirmaNovaSenha){
+    if (novaSenha !== confirmaNovaSenha) {
       throw new Error("Confirmação de senha não confere");
     }
 
-    if(id != usuarioLogado.id){
-      throw new Error('Não é possível alterar senha de outro usuário');
+    if (id != usuarioLogado.id) {
+      throw new Error("Não é possível alterar senha de outro usuário");
     }
 
     const usuarioDB = await this.repo.buscarPorId(id);
     if (!usuarioDB) {
       throw new Error("Usuário não existe");
     }
-  
+
     const mesmaSenha = await this.cripto.comparar(senhaAtual, usuarioDB.senha);
     if (!mesmaSenha) {
       throw new Error("Senha incorreta");
