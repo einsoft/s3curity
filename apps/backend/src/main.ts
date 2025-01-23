1d0d24fe762e085c0f567c2a51ffc3de7638468a
Albert E. Souza <einsoft@gmail.com>
2025-01-22 22:47:42 -0300

    feat: integrado Swagger para documentação de API e atualizadas dependências

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import ErrorFilter from './error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação Swagger')
+    .setDescription('Documentação Swagger para a API de S3curity.')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(process.env.PORT ?? 4000);
 }
