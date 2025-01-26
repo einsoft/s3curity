e9b388a71ac43668176b4aad7eb05dab60f81c2c
Albert E. Souza <einsoft@gmail.com>
2025-01-25 23:13:40 -0300

    feat: add perfil module exports for improved profile management

---
+++ packages/core/src/perfil/index.ts
@@ -0,0 +1,6 @@
+import Perfil from "./model/Perfil";
+import RepositorioPerfil from "./provider/RepositorioPerfil";
+import CadastrarPerfil from "./service/CadastrarPerfil";
+
+export type { Perfil };
+export { RepositorioPerfil, CadastrarPerfil };
