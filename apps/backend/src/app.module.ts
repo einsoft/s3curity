8ae6aa4750a1709cbd9822db84478219b5c7b12e
Albert E. Souza <einsoft@gmail.com>
2025-01-27 06:01:36 -0300

    refactor: :recycle: movida o app para raiz do Nest

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AuthModule } from '../auth/auth.module';
-import { DbModule } from '../db/db.module';
-import { PerfilModule } from '../perfil/perfil.module';
-import { UsuarioModule } from '../usuario/usuario.module';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { DbModule } from './db/db.module';
+import { PerfilModule } from './perfil/perfil.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
