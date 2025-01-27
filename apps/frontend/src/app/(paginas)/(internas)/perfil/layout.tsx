f0f12268458ff3a0ee6eaa090d2216da55405037
Albert E. Souza <einsoft@gmail.com>
2025-01-27 05:21:50 -0300

    feat: :sparkles: perfil: layout holly grail + menu lateral

--- apps/frontend/src/app/(paginas)/(internas)/perfil/layout.tsx
+++ apps/frontend/src/app/(paginas)/(internas)/perfil/layout.tsx
@@ -5,17 +5,18 @@ import MenuSidebar from "@/src/components/shared/MenuSidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col mt-12">
+    <>
       <header>
         <Cabecalho />
       </header>
-
-      <div className="flex-1 flex flex-col sm:flex-row">
-        <main className="flex-1 p-4">{children}</main>
-        <nav className="order-first sm:w-64">
-          <MenuSidebar />
-        </nav>
+      <div className="min-h-screen flex flex-col mt-12">
+        <div className="flex-1 flex flex-col sm:flex-row">
+          <main className="flex-1 p-4 bg-[#a1a1aa1a]">{children}</main>
+          <nav className="order-first sm:w-64">
+            <MenuSidebar />
+          </nav>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- apps/frontend/src/components/shared/MenuSidebar.tsx
+++ apps/frontend/src/components/shared/MenuSidebar.tsx
@@ -4,11 +4,11 @@ import { IconList, IconSettings } from "@tabler/icons-react";
 
 export default function MenuSidebar() {
   return (
-    <div className="min-w-[240px] p-4 rounded-lg">
+    <div className="min-w-[240px] pl-4 rounded-lg">
       <nav className="flex flex-col gap-1 font-sans text-base font-normal text-blue-gray-700">
         <div
           role="button"
-          className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="flex items-center w-full p-3 leading-tight transition-all rounded-l-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 bg-[#a1a1aa1a]"
         >
           <div className="grid mr-4 place-items-center">
             <IconSettings />
