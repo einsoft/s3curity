e668d117a808e24a0975196e10c280a64e5c0e2a
Albert E. Souza <einsoft@gmail.com>
2025-01-27 05:29:03 -0300

    feat: :sparkles: atualização/configuração do plugin do prettier para tailwind

--- apps/frontend/src/components/shared/MenuSidebar.tsx
+++ apps/frontend/src/components/shared/MenuSidebar.tsx
@@ -4,22 +4,22 @@ import { IconList, IconSettings } from "@tabler/icons-react";
 
 export default function MenuSidebar() {
   return (
-    <div className="min-w-[240px] pl-4 rounded-lg">
-      <nav className="flex flex-col gap-1 font-sans text-base font-normal text-blue-gray-700">
+    <div className="min-w-[240px] rounded-lg pl-4">
+      <nav className="text-blue-gray-700 flex flex-col gap-1 font-sans text-base font-normal">
         <div
           role="button"
-          className="flex items-center w-full p-3 leading-tight transition-all rounded-l-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 bg-[#a1a1aa1a]"
+          className="hover:bg-blue-gray-50 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:text-blue-gray-900 active:bg-blue-gray-50 active:text-blue-gray-900 flex w-full items-center rounded-l-lg bg-[#a1a1aa1a] p-3 text-start leading-tight outline-none transition-all hover:bg-opacity-80 focus:bg-opacity-80 active:bg-opacity-80"
         >
-          <div className="grid mr-4 place-items-center">
+          <div className="mr-4 grid place-items-center">
             <IconSettings />
           </div>
           Gerenciar
         </div>
         <div
           role="button"
-          className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="hover:bg-blue-gray-50 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:text-blue-gray-900 active:bg-blue-gray-50 active:text-blue-gray-900 flex w-full items-center rounded-lg p-3 text-start leading-tight outline-none transition-all hover:bg-opacity-80 focus:bg-opacity-80 active:bg-opacity-80"
         >
-          <div className="grid mr-4 place-items-center">
+          <div className="mr-4 grid place-items-center">
             <IconList />
           </div>
           Visualizar
