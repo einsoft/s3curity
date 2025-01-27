5eba82c246b93fc58cc37e8879ed75d0a79ce3dc
Albert E. Souza <einsoft@gmail.com>
2025-01-27 09:51:29 -0300

    feat: add permission management services to index.ts

--- packages/core/src/permissao/index.ts
+++ packages/core/src/permissao/index.ts
@@ -1,3 +1,13 @@
 import Permissao from "./model/Permissao";
+import AtualizarPermissao from "./service/AtualizarPermissao";
+import CadastrarPermissao from "./service/CadastrarPermissao";
+import ExcluirPermissao from "./service/ExcluirPermissao";
+import ListarPermissao from "./service/ListarPermissao";
 
 export type { Permissao };
+export {
+  AtualizarPermissao,
+  CadastrarPermissao,
+  ExcluirPermissao,
+  ListarPermissao,
+};
