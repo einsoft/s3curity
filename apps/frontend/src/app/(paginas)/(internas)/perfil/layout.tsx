18fe78ed537eafa1f49748b8849b1b6621965d54
Albert E. Souza <einsoft@gmail.com>
2025-01-28 21:46:54 -0300

    feat: melhora o layout da página de perfil com novos elementos de navegação

--- apps/frontend/src/app/(paginas)/(internas)/perfil/layout.tsx
+++ apps/frontend/src/app/(paginas)/(internas)/perfil/layout.tsx
@@ -7,8 +7,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col mt-12">
       <Cabecalho />
       <div className="flex flex-1 flex-row">
-        <main className="flex-1 p-4">{children}</main>
-        <nav className="order-first w-64 bg-black p-4">Navigation</nav>
+        <main className="flex-1">{children}</main>
+        <nav className="order-first w-64 bg-black/60">
+          <div className="ml-3 rounded-l-lg bg-[#121212] p-2">Listar</div>
+          <div className="ml-3 rounded-l-lg p-2">Adicionar</div>
+        </nav>
       </div>
     </div>
   );
