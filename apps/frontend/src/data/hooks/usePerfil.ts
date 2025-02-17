ddcaac46cfd7b56b0f831708114cc599a74e32c6
Albert E. Souza <einsoft@gmail.com>
2025-02-17 09:17:27 -0300

    feat: remove import desnecessário do useRouter no hook usePerfil

--- apps/frontend/src/data/hooks/usePerfil.ts
+++ apps/frontend/src/data/hooks/usePerfil.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
-import { useRouter } from "next/router";
 
 import { useToastContext } from "../contexts/ToastContext";
 import useAPI from "./useAPI";
